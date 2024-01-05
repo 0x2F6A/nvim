38fc3f4a0048075258060484ee20e717569287a4
MysticalDevil <jinpeng7981@outlook.com>
2024-01-05 23:00:50 +0800

    feat: Add some lsp config(which supported inlay hints)

--- lua/devil/plugins/configs/lspconfig.lua
+++ lua/devil/plugins/configs/lspconfig.lua
@@ -1,13 +1,35 @@
 local M = {}
 local utils = require("devil.core.utils")
+local lspconfig = require("lspconfig")
 
-vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+local inlay_hint = vim.lsp.inlay_hint
+
+function M.set_inlay_hints(client, bufnr)
+  if not client then
+    vim.notify_once("LSP inlay hints attached failed: nil client.", vim.log.levels.ERROR)
+    return
+  end
+
+  if client.name == "zls" then
+    vim.g.zig_fmt_autosave = 1
+  end
+
+  if client.supports_method("textDocument/inlayHint") or client.server_capabilities.inlayHintProvider then
+    inlay_hint.enable(bufnr, true)
+  end
+end
 
 M.on_attach = function(client, bufnr)
   client.server_capabilities.documentFormattingProvider = false
   client.server_capabilities.documentRangeFormattingProvider = false
 
+  M.set_inlay_hints(client, bufnr)
+
   utils.load_mappings("lspconfig", { buffer = bufnr })
+
+  vim.api.nvim_set_option_value("formatexpr", "v:lua.require'conform'.formatexpr()", { buf = bufnr })
+  vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
+  vim.api.nvim_set_option_value("tagfunc", "v:lua.vim.lsp.tagfunc", { buf = bufnr })
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -30,12 +52,20 @@ M.capabilities.textDocument.completion.completionItem = {
   },
 }
 
-require("lspconfig").lua_ls.setup({
+local runtime_path = vim.split(package.path, ";", {})
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/>/init.lua")
+
+lspconfig.lua_ls.setup({
   on_attach = M.on_attach,
   capabilities = M.capabilities,
 
   settings = {
     Lua = {
+      runtime = {
+        version = "LuaJIT",
+        path = runtime_path,
+      },
       diagnostics = {
         globals = { "vim" },
       },
@@ -48,6 +78,178 @@ require("lspconfig").lua_ls.setup({
         maxPreload = 100000,
         preloadFileSize = 10000,
       },
+      hint = {
+        enable = true,
+        arrayIndex = "Auto",
+        await = true,
+        paramName = "All",
+        paramType = true,
+        semicolon = "SameLine",
+        setType = false,
+      },
+    },
+  },
+})
+
+lspconfig.clangd.setup((function()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.offsetEncoding = { "utf-16" } ---@diagnostic disable-line
+
+  return {
+    on_attach = M.on_attach,
+    capabilities = capabilities,
+
+    settings = {
+      clangd = {
+        InlayHints = {
+          Designators = true,
+          Enabled = true,
+          ParameterNames = true,
+          DeducedTypes = true,
+        },
+        fallbackFlags = { "-std=c++20" },
+      },
+    },
+  }
+end)())
+
+lspconfig.gopls.setup({
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+
+  settings = {
+    gopls = {
+      experimentalPostfixCompletions = true,
+      analyses = {
+        shadow = true,
+        fieldalignment = true,
+        nilness = true,
+        unusedparams = true,
+        unusedwrite = true,
+        useany = true,
+      },
+      gofumpt = true,
+      hints = {
+        rangeVariableTypes = true,
+        parameterNames = true,
+        constantValues = true,
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        functionTypeParameters = true,
+      },
+      codelenses = {
+        gc_details = false,
+        generate = true,
+        regenerate_cgo = true,
+        run_govulncheck = true,
+        test = true,
+        tidy = true,
+        upgrade_dependency = true,
+        vendor = true,
+      },
+      usePlaceholders = true,
+      completeUnimported = true,
+      staticcheck = true,
+      directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+      semanticTokens = true,
+    },
+  },
+})
+
+lspconfig.rust_analyzer.setup({
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+
+  settings = {
+    rust_analyzer = {
+      checkOnSave = {
+        allFeatures = true,
+        overrideCommand = {
+          "cargo",
+          "clippy",
+          "--workspace",
+          "--message-format=json",
+          "--all-targets",
+          "--all-features",
+        },
+      },
+      cargo = {
+        loadOutDirsFromCheck = true,
+      },
+      procMacro = {
+        enable = true,
+      },
+      inlayHints = {
+        bindingModeHints = {
+          enable = false,
+        },
+        chainingHints = {
+          enable = true,
+        },
+        closingBraceHints = {
+          enable = true,
+          minLines = 25,
+        },
+        closureReturnTypeHints = {
+          enable = "never",
+        },
+        lifetimeElisionHints = {
+          enable = "never",
+          useParameterNames = false,
+        },
+        maxLength = 25,
+        parameterHints = {
+          enable = true,
+        },
+        reborrowHints = {
+          enable = "never",
+        },
+        renderColons = true,
+        typeHints = {
+          enable = true,
+          hideClosureInitialization = false,
+          hideNamedConstructor = false,
+        },
+      },
+    },
+  },
+})
+
+lspconfig.zls.setup({
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+
+  settings = {
+    zls = {
+      enable_snippets = true,
+      enable_argument_placeholders = true,
+      enable_ast_check_diagnostics = true,
+      enable_build_on_save = true,
+      enable_autofix = false,
+      semantic_tokens = "full",
+      enable_inlay_hints = true,
+      inlay_hints_show_variable_type_hints = true,
+      inlay_hints_show_parameter_name = true,
+      inlay_hints_show_builtin = true,
+      inlay_hints_exclude_single_argument = true,
+      inlay_hints_hide_redundant_param_names = false,
+      inlay_hints_hide_redundant_param_names_last_token = false,
+      warn_style = false,
+      highlight_global_var_declarations = false,
+      dangerous_comptime_experiments_do_not_enable = false,
+      skip_std_references = false,
+      prefer_ast_check_as_child_process = true,
+      record_session = false,
+      record_session_path = nil,
+      replay_session_path = nil,
+      builtin_path = nil,
+      zig_lib_path = nil,
+      zig_exe_path = nil,
+      build_runner_path = nil,
+      global_cache_path = nil,
+      build_runner_global_cache_path = nil,
+      completions_with_replace = true,
     },
   },
 })
